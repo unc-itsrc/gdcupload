95b752a70604bff153067ce6eab688c068a9b3f6
McGee, John <johnmcge@ad.unc.edu>
2019-04-11 09:53:05 -0400

    Add project files.

---
+++ upload2gdc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace upload2gdc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
